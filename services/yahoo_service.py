4eb1a7969b0425ae19325c473e6d1ab282ef0cbd
fevziyazgan <34141431+fevziyazgan@users.noreply.github.com>
2026-06-14 18:45:13 +0300

    Update yahoo_service.py

--- services/yahoo_service.py
+++ services/yahoo_service.py
@@ -1,7 +1,11 @@
 import yfinance as yf
-def get_price(ticker):
+def get_price(
+    ticker
+):
     try:
-        data = yf.Ticker(ticker)
+        data = yf.Ticker(
+            ticker
+        )
         hist = data.history(
             period="5d"
         )
@@ -9,7 +13,9 @@ def get_price(ticker):
             return None
         return round(
             float(
-                hist["Close"].iloc[-1]
+                hist[
+                    "Close"
+                ].iloc[-1]
             ),
             4
         )
@@ -18,3 +24,36 @@ def get_price(ticker):
             f"YAHOO ERROR {ticker}: {e}"
         )
         return None
+def get_usdtry():
+    return get_price(
+        "USDTRY=X"
+    )
+def get_bist100():
+    return get_price(
+        "XU100.IS"
+    )
+def get_us10y():
+    return get_price(
+        "^TNX"
+    )
+def get_gold_ounce():
+    return get_price(
+        "GC=F"
+    )
+def get_gram_gold():
+    ounce = get_gold_ounce()
+    usdtry = get_usdtry()
+    if (
+        ounce is None
+        or
+        usdtry is None
+    ):
+        return None
+    gram_gold = (
+        ounce
+        / 31.1035
+    ) * usdtry
+    return round(
+        gram_gold,
+        2
+    )
